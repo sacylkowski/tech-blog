9e09ee5e3a684b95d962662c15a2e925d8754687
sacylkowski <sacylkowski@gmail.com>
2022-05-08 21:31:15 -0500

    homepage displays

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require("path");
 // const session = require("express-session");
-// const exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,22 +26,22 @@ const PORT = process.env.PORT || 3001;
 
 // const helpers = require("./utils/helpers");
 // // passing the helpers
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ });
 
-// app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // turn on routes
 app.use(routes);
 
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
