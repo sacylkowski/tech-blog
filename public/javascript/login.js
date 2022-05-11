1294a99c241f98e3e2d93b6d9abae3c236aace04
sacylkowski <sacylkowski@gmail.com>
2022-05-10 20:16:51 -0500

    signup and login functions added

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,4 +1,4 @@
-function signupForm(event) {
+async function signupForm(event) {
     event.preventDefault();
 
     // grabbing the data from the form
@@ -8,7 +8,7 @@ function signupForm(event) {
 
     // if all information is provided
     if (username && email && password) {
-        fetch("/api/users", {
+        await fetch("/api/users", {
             method: "post",
             body: JSON.stringify({
                 username,
@@ -16,8 +16,39 @@ function signupForm(event) {
                 password
             }),
             headers: { "Content-Type": "application/json" }
-         }).then((response) => {console.log(response)})
+         });
+         if (response.ok) {
+             console.log("success");
+         } else {
+             alert(response.statusText);
+         }
+    }
+}
+
+async function loginForm(event) {
+    event.preventDefault();
+
+    // grabbing the data from the form
+    const email = document.querySelector("#email-signup").value.trim();
+    const password = document.querySelector("#password-signup").value.trim();
+
+    // if all information is provided
+    if (email && password) {
+        const response = await fetch("/api/users/login", {
+            method: "post",
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { "Content-Type": "application/json" }
+         });
+         if (response.ok) {
+             document.location.replace("/");
+         } else {
+             alert(response.statusText);
+         }
     }
 }
 
 document.querySelector(".signup-form").addEventListener("submit", signupForm);
+document.querySelector(".login-form").addEventListener("submit", signupForm);
