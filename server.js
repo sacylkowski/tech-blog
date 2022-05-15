6f3b0c73d55e9d118a679db75a74e71568354dc4
sacylkowski <sacylkowski@gmail.com>
2022-05-15 12:39:47 -0500

    added helpers

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ const sess = {
 
 app.use(session(sess));
 
-// const helpers = require("./utils/helpers");
+const helpers = require("./utils/helpers");
 // // passing the helpers
-const hbs = exphbs.create({ });
+const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
